620e1e65878a2036a503e970bea46ef5523b2b39
chenrui <ruiweilai@163.com>
2024-07-19 16:06:11 +0800

    fix:1.修复运行项目打开厂家网站 2.新增表格组件advance-table 3.新增分页组件

---
+++ web/src/components/AdvancedTable/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <el-table 
+    :data="tableData"
+    :columns="columns"
+    :row-key="row => row.id"
+    header-align="center"
+    @select="onTableSelect"
+    @select-all="onTableSelectAll" 
+    border style="width: 80%">
+    <template #empty>
+      <el-empty description="未查询到相关数据" />
+    </template>
+    <el-table-column
+      v-if="index"
+      type="index"
+      width="50"
+      label="序号"
+      align="center"
+    >
+      <template #default="scope">
+        {{ scope.$index + 1 + (listQuery.page - 1) * listQuery.limit }}
+      </template>
+    </el-table-column>
+    <el-table-column
+      v-if="selection"
+      reserve-selection
+      type="selection"
+      width="45"
+    />
+    <el-table-column
+      v-for="(column, index)  in columns"
+      :key="index"
+      :label="column.label"
+      :prop="column.prop"
+      :formatter="column.formatter"
+      align="center"
+    >
+    <!-- 自定义单元格内容 -->
+    <template v-if="column.slot" #default="scope">
+        <slot :name="column.slot" :row="scope.row" />
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="status"
+      label="操作"
+      v-if="statusData"
+      :width="statusWidth"
+      align="left"
+    >
+      <template #default="scope">
+          <template v-for="(btn, index) in statusData">
+              <template v-if="btn.visible ? btn.visible(scope, btn) : true">
+                  <el-button :type="btn.type" @click="btn.handleClick && btn.handleClick(scope)" :key="index">{{  btn.name }}</el-button>
+              </template>
+          </template>
+      </template>
+    </el-table-column>
+  </el-table>
+  <pagination
+    v-if="listQuery && pagination"
+    :total="listQuery.total"
+    :page="listQuery.page"
+    :limit="listQuery.limit"
+    :fix-foot="listQuery.fixFoot"
+    @pagination="handlePagination"
+  />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CommonTable',
+  props: {
+    columns: {
+      default: [],
+      type: Array,
+      required: true
+    },
+    tableData: {
+      default: [],
+      type: Array,
+      required: true
+    },
+    listQuery:{
+      default: null,
+      type:Object,
+      required: true
+    },
+    selection: { default: false, type: Boolean },
+    statusData: { default: null, type: Array },
+    statusWidth: { default: null, type: String },
+    index: { default: false, type: Boolean },
+    pagination: { default: null, type: Function },
+  },
+  emits: ['update:tableData', 'update:listQuery'],
+  setup(props, { emit }) {
+    const handlePagination = (page, limit) => {
+      emit('update:listQuery', { ...props.listQuery, page, limit });
+    };
+
+    const onTableSelect = (selection, row) => {
+      const selectedStatus = selection.length && selection.includes(row);
+      props.selectionRow && props.selectionRow(selection, row, selectedStatus);
+      };
+    const onTableSelectAll = (selection) => {
+      props.selectionAll && props.selectionAll(selection);
+    };
+
+    return {
+      handlePagination,
+      onTableSelectAll,
+      onTableSelect
+    };
+  }
+
+  
+});
+</script>
+
+<style scoped>
+</style>
