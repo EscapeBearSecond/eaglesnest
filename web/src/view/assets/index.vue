653eb9d0f0cd46804c1250bb7a42ab409020d524
chenrui <ruiweilai@163.com>
2024-07-23 14:51:38 +0800

    fix: 新增资产列表

--- web/src/view/assets/index.vue
+++ web/src/view/assets/index.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { ref, reactive } from 'vue' 
 const searchInfo = reactive({
-    name:''
+    assetName:''
 })
 const tableColumns = reactive([
-       { label:'名称', prop:'taskName'},
-       { label:'描述', prop:'taskDesc'},
-       { label:'目标', prop:'targetIp'},
-       { label:'任务计划', prop:'taskPlan'},
-       { label:'状态', prop:'status'},
-       { label:'策略', prop:'planConfig'},
+       { label:'名称', prop:'assetName'},
+       { label:'IP', prop:'assetIP'},
+       { label:'归属', prop:'assetArea'},
+       { label:'类型', prop:'assetType'},
+       { label:'价值', prop:'manufacturer'},
+       { label:'备注', prop:'desc'},
     ])
 const tableData = ref([])
 const listQuery = reactive({
@@ -19,43 +19,30 @@ const listQuery = reactive({
    })
 const statusData = reactive([
   {
-      name: "停止",
-      type: "primary",
-      icon: "edit",
-      handleClick: (scope) => handleStop(scope.row),
-      // visible: (scope, btn) => scope.row.name == 'aaa' 测试按钮根据情况显示 
-  },
-  {
       name: "删除",
       type: "primary",
       icon: "edit",
-      handleClick: (scope) => handleDel(scope.row),
-      // visible: (scope, btn) => scope.row.name == 'aaa' 测试按钮根据情况显示 
-  },
-  {
-      name: "生成报告",
-      type: "primary",
-      icon: "edit",
-      handleClick: (scope) => handleReport(scope.row),
-      // visible: (scope, btn) => scope.row.name == 'aaa' 测试按钮根据情况显示 
+      handleClick: (scope) => handleDel(scope.row), 
   }
 ])
 
 const addDialogFlag = ref(false)
-const dialogTitle = ref('新建扫描任务')
+const dialogTitle = ref('新增资产')
 
 const onSubmit = (searchInfo) => {}
 
 const onReset = () => {
-  searchInfo.value = {}
+  searchInfo.assetName.value = ""
 }
 
-const  createScanTask = ()=> { addDialogFlag.value = true}
-
-const  handleStop = (e) => { console.log(e);}
+const  createAsset = ()=> { 
+  addDialogFlag.value = true;
+}
 const  handleDel = (e) => { console.log(e);}
-const  handleReport = (e) => { console.log(e);}
 
+const pagination = (listQuery)=> {
+
+}
 </script>
 <template>
     <div>
@@ -67,8 +54,8 @@ const  handleReport = (e) => { console.log(e);}
          >
          <el-form-item label="名称">
             <el-input
-               v-model="searchInfo.name"
-               placeholder="任务名称"
+               v-model="searchInfo.assetName"
+               placeholder="资产名称"
             />
          </el-form-item>
          <el-form-item>
@@ -86,11 +73,7 @@ const  handleReport = (e) => { console.log(e);}
       </div>
       <div class="gva-table-box">
         <div class="gva-btn-list">
-          <el-button
-              type="primary"
-              icon="plus"
-              @click="createScanTask"
-          >创建扫描</el-button>
+          <el-button type="primary" icon="plus" @click="createAsset">新增资产</el-button>
         </div>
         <advance-table
           :columns = "tableColumns"
@@ -103,15 +86,17 @@ const  handleReport = (e) => { console.log(e);}
         </advance-table>
       </div>
       <el-dialog
-         v-model="addDialogFlag"
-         :title="dialogTitle"
-         style="padding-left:50px"
-      >
-      <div>
-        创建扫描任务
-      </div>
-    </el-dialog>
+    v-model="addDialogFlag"
+    :title="dialogTitle"
+    style="padding-left:50px"
+    @update:modelValue="val => addDialogFlag = val"
+  >
+    <div>
+      新增资产
+    </div>
+  </el-dialog>
   </div>
+
 </template>
 <style lang='scss' scoped>
   
