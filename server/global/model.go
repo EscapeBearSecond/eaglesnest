a0d6f569325ec6e4f49f4098a615115270b7c686
丁元广 <dinyuanguang@163.com>
2024-07-29 15:51:44 +0800

    fix: 修复字典显示问题

--- server/global/model.go
+++ server/global/model.go
@@ -7,8 +7,8 @@ import (
 )
 
 type GvaModel struct {
-	ID        uint           `gorm:"primaryKey" json:"id"` // 主键ID
-	CreatedAt time.Time      `json:"createdAt"`            // 创建时间
-	UpdatedAt time.Time      `json:"updatedAt"`            // 更新时间
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`       // 删除时间
+	ID        uint           `gorm:"primaryKey" json:"ID"` // 主键ID
+	CreatedAt time.Time      // 创建时间
+	UpdatedAt time.Time      // 更新时间
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"` // 删除时间
 }
