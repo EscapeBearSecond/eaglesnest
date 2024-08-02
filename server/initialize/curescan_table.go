1661c20b12421e574d3aae84f58422b4584e9964
鲁兆聪 <blankstark@163.com>
2024-08-02 17:28:24 +0800

    refactor: 1. 登录页增加初始化数据库按钮 2. 后端启动代码微调

--- server/initialize/curescan_table.go
+++
@@ -1,8 +0,0 @@
-package initialize
-
-func MigrateTables() {
-	// 	err := global.GVA_DB.AutoMigrate(&curescan.PortScan{}, &curescan.OnlineCheck{}, &curescan.JobResultItem{})
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-}
