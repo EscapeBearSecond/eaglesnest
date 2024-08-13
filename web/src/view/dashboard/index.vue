b45e13eb8e8a87e46a8dfb17c316ea5e88800af5
chenrui <ruiweilai@163.com>
2024-08-13 17:00:36 +0800

    fix:首页修改

--- web/src/view/dashboard/index.vue
+++ web/src/view/dashboard/index.vue
@@ -6,13 +6,39 @@
           <div class="medium-char bg">3</div>
           <div class="medium-char bg">4</div>
       </div>
-      <div class="task-content bg-t">
-         <span>1</span>
-         <span>2</span>
-         <span>3</span>
-         <span>4</span>
-         <span>5</span>
-         <span>6</span>
+      <div class="task-content">
+         <span> <el-statistic title="Daily active users" :value="268500" /></span>
+         <span>
+            <el-statistic :value="138">
+              <template #title>
+                <div style="display: inline-flex; align-items: center">
+                  Ratio of men to women
+                  <el-icon style="margin-left: 4px" :size="12">
+                    <Male />
+                  </el-icon>
+                </div>
+              </template>
+              <template #suffix>/100</template>
+            </el-statistic>
+          </span>
+         <span>
+          <el-statistic title="Total Transactions" :value="outputValue" />
+         </span>
+         <span>
+          <el-statistic title="Feedback number" :value="562">
+            <template #suffix>
+              <el-icon style="vertical-align: -0.125em">
+                <ChatLineRound />
+              </el-icon>
+            </template>
+          </el-statistic>
+         </span>
+         <span>
+          <el-statistic title="Daily active users" :value="268500" />
+        </span>
+        <span>
+          <el-statistic title="Daily active users" :value="268500" />
+        </span>
       </div>
       <div class="top-content bg-f">
         <div>1</div>
@@ -22,7 +48,19 @@
 </template>
 
 <script setup>
+import { ref, reactive } from 'vue'
+import { useTransition } from '@vueuse/core'
+defineOptions({
+  name: 'Index'
+})
 
+
+const source = ref(0)
+const outputValue = useTransition(source, {
+  duration: 1500,
+})
+
+source.value = 172000
 </script>
 
 <style lang="scss" scoped>
