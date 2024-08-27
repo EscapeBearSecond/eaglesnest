7c5bf36696e62b7d48560fdab478316bfecc8af7
丁元广 <dinyuanguang@163.com>
2024-08-27 14:45:39 +0800

    fix: 初始化修复

--- server/initialize/cs_task.go
+++ server/initialize/cs_task.go
@@ -8,12 +8,14 @@ import (
 
 // RecoverTask 将因系统出现崩溃或异常而导致的任务恢复 将执行中改为失败
 func RecoverTask() {
-	err := global.GVA_DB.Exec("update cs_task set status = ? where status = ?", common.Failed, common.Running).Error
-	if err != nil {
-		global.GVA_LOG.Error("RecoverTask common task failed", zap.Error(err))
-	}
-	err = global.GVA_DB.Exec("update cs_task set status = ? where status = ?", common.TimeStopped, common.TimeRunning).Error
-	if err != nil {
-		global.GVA_LOG.Error("RecoverTask timing task failed", zap.Error(err))
+	if global.GVA_DB != nil {
+		err := global.GVA_DB.Exec("update cs_task set status = ? where status = ?", common.Failed, common.Running).Error
+		if err != nil {
+			global.GVA_LOG.Error("RecoverTask common task failed", zap.Error(err))
+		}
+		err = global.GVA_DB.Exec("update cs_task set status = ? where status = ?", common.TimeStopped, common.TimeRunning).Error
+		if err != nil {
+			global.GVA_LOG.Error("RecoverTask timing task failed", zap.Error(err))
+		}
 	}
 }
