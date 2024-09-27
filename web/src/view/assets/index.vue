cd3535a1262b87ea9139f90c93babc62b318c356
chenrui <ruiweilai@163.com>
2024-09-27 14:07:02 +0800

    fix:资产端口样式修改

--- web/src/view/assets/index.vue
+++ web/src/view/assets/index.vue
@@ -9,11 +9,16 @@
        >
          <el-form-item label="名称">
            <el-input
-             v-model="searchInfo.areaName"
-             placeholder="请输入区域名称"
+             v-model="searchInfo.assetIp"
+             placeholder="请输入IP地址"
            />
          </el-form-item>
-
+         <el-form-item label="设备类型"  class="sec-lab"> 
+              <el-select v-model="searchInfo.assetType" placeholder="请选择设备类型">
+                  <el-option label="全部" value=""></el-option>
+                  <el-option v-for="(tagOne, key) in tagList.tag1" :label="tagOne" :value="tagOne" :key="key" />
+              </el-select>
+          </el-form-item>
          <el-form-item>
            <el-button
              type="primary"
@@ -28,11 +33,11 @@
        </el-form>
      </div>
       <div class="gva-btn-list">
-        <el-button
+        <!-- <el-button
           type="primary"
           icon="plus"
-          @click="addArea"
-        >新增区域</el-button>
+          @click="addForm"
+        >新增资产</el-button> -->
       </div>
       <advance-table
         :columns="tableColumns"
@@ -43,65 +48,38 @@
         :changePageSize="changeSize"
         :index="true"
       >
+      <template v-slot:customAreaName="slotProps">
+        <!-- 自定义的字段 -->
+        <span> 
+          <el-tag effect="dark" type="warning">{{ getAreaName(slotProps.row.assetArea) }}</el-tag>
+        </span>
+      </template>
+      <template v-slot:customProt="slotProps">
+        <el-popover
+          placement="bottom-start"
+          title=""
+          :width="200"
+          trigger="hover"
+          :content="getPortStr(slotProps.row.openPorts)"
+        >
+        <!-- 自定义的字段 -->
+        <template #reference>
+            <el-tag effect="dark"  type="primary" >{{  getPortsTag(slotProps.row.openPorts) }}</el-tag>
+        </template>
+      </el-popover>
+      </template>
       </advance-table>
 
     </div>
-    <el-drawer
-      v-model="districtInfoDialog"
-      size="45%"
-      :show-close="false"
-      :close-on-press-escape="false"
-      :close-on-click-modal="false"
-    >
-      <template #header>
-        <div class="flex justify-between items-center">
-          <span class="text-lg">区域</span>
-          <div>
-            <el-button @click="closeAddUserDialog">取 消</el-button>
-            <el-button
-              type="primary"
-              @click="enterAddUserDialog"
-            >确 定</el-button>
-          </div>
-        </div>
-      </template>
-
-      <el-form
-        ref="form"
-        :rules="rules"
-        :model="districtInfo"
-        label-width="80px"
-      >
-        <el-form-item
-          label="区域名称"
-          prop="areaName"
-        >
-          <el-input v-model="districtInfo.areaName" />
-        </el-form-item>
-        <el-form-item
-          label="IP范围"
-          prop="areaIpStr"
-        >
-        <el-input type="textarea" :rows="6"  v-model="districtInfo.areaIpStr" placeholder="参考：10.0.0.1/24, 10.0.0.1 ~ 10.0.0.255 多个地址段请用逗号分隔"  />
-        </el-form-item>
-        <el-form-item
-          label="备注"
-        >
-        <el-input v-model="districtInfo.areaDesc" />
-        </el-form-item>
-      </el-form>
-    </el-drawer>
+  
   </div>
 </template>
 
 <script setup>
-import { getAreaList, createArea, editArea, delArea } from "@/api/area"
-
-import { getAuthorityList } from '@/api/authority'
-
-
-import { ref, watch, reactive } from 'vue'
-import { ElMessage, ElMessageBox } from 'element-plus'
+import { getListApi } from "@/api/assets"
+import { getAreaList } from "@/api/area"
+import { getTemplateTagList } from '@/api/template'
+import { ref, reactive } from 'vue'
 
 defineOptions({
   name: 'Area',
@@ -135,12 +113,19 @@ const listQuery = reactive({
    total: 0,
    pageSize: 10,
 })
-
 const changeSize = (e) => {
   listQuery.page = 1
   listQuery.pageSize = e
   getTableData()
 }
+
+//获取四层筛选
+const tagList = ref({})
+const getTemplateTagData = async () => {
+     const data = await getTemplateTagList()
+     tagList.value = data.data
+}
+getTemplateTagData()
 
 const handleCurrentChange = (val) => {
   page.value = val
@@ -148,20 +133,7 @@ const handleCurrentChange = (val) => {
 }
 
 // 操作
-const statusData = reactive([
-   {
-      name: "修改",
-      type: "primary",
-      icon: "Edit",
-      handleClick: (scope) => handleClickUpdate(scope.row), 
-  },
-  {
-      name: "删除",
-      type: "danger",
-      icon: "Delete",
-      handleClick: (scope) => handleClickDelete(scope.row), 
-  }
-])
+const statusData = reactive([])
 
 const searchInfo = ref({})
 const onSubmit = () => {
@@ -170,13 +142,13 @@ const onSubmit = () => {
  }
 
  const onReset = () => {
-   searchInfo.value = {}  
-   getTableData() 
+   searchInfo.value = {}
+   getTableData()   
  }
 
 // 查询
 const getTableData = async() => {
-  const table = await getAreaList({
+  const table = await getListApi({
       page: listQuery.page,
       pageSize: listQuery.pageSize,
       ...searchInfo.value,
@@ -186,135 +158,51 @@ const getTableData = async() => {
       listQuery.total = table.data.total;
       listQuery.page = table.data.page;
       listQuery.pageSize = table.data.pageSize;
-    }
+    }    
 }
+const areaList = ref([])
+const getAreaData = async() => {
+  const table = await getAreaList({
+      page: 1,
+      pageSize: 100,
+  });
 
-watch(() => tableData.value, () => {
-  setAuthorityIds()
-})
+  areaList.value = table.data.list
+}
 
 const initPage = async() => {
   getTableData()
-  const res = await getAuthorityList({ page: 1, pageSize: 999 })
-  setOptions(res.data.list)
+  getAreaData()
 }
 
 initPage()
 
-
-const setAuthorityIds = () => {
-  tableData.value && tableData.value.forEach((user) => {
-    user.authorityIds = user.authorities && user.authorities.map(i => {
-      return i.authorityId
-    })
-  })
-}
-
-
-
-const authOptions = ref([])
-const setOptions = (authData) => {
-  authOptions.value = []
-  setAuthorityOptions(authData, authOptions.value)
-}
-
-const handleClickDelete = async(row) => {
-  ElMessageBox.confirm('确定要删除吗?', '提示', {
-    confirmButtonText: '确定',
-    cancelButtonText: '取消',
-    type: 'warning'
-  }).then(async() => {
-    const res = await delArea({ id: row.ID })
-    if (res.code === 0) {
-      ElMessage.success('删除成功')
-      await getTableData()
-    }
-  })
-}
-
-// 弹窗相关
-const districtInfo = ref({
-  areaName:"",
-  areaIp:"",
-  areaIpStr:"",
-  areaDesc:"",
-})
-
 const tableColumns = reactive([
-    { label:'区域名称', prop:'areaName'},
-    { label:'IP范围', prop:'areaIp'},
-    { label:'备注', prop:'areaDesc'},
+    { label:'名称', prop:'assetName'},
+    { label:'IP', prop:'assetIp'},
+    // { label:'区域', prop:'assetArea', slot: 'customAreaName'},
+    { label:'设备类型', prop:'assetType'},
+    { label:'开放端口', prop:'openPorts', slot: 'customProt'},
 ])
 
-const rules = ref({
-  areaName: [
-    { required: true, message: '请输入区域名称', trigger: 'blur' },
-  ],
-  areaIpStr: [
-    { required: true, message: '请输入IP范围', trigger: 'blur' },
-  ],
-  
-})
-const form = ref(null)
-const enterAddUserDialog = async() => {
-  
-  form.value.validate(async valid => {
-    if (valid) {
-      const req = {
-        ...districtInfo.value
-      }
-      req.areaIp = getIpArr(req.areaIpStr)
-      if (dialogFlag.value === 'add') {
-        const res = await createArea(req)
-        if (res.code === 0) {
-          ElMessage({ type: 'success', message: '创建成功' })
-          await getTableData()
-          closeAddUserDialog()
-        }
-      }
-      if (dialogFlag.value === 'edit') {
-        const res = await editArea(req)
-        if (res.code === 0) {
-          ElMessage({ type: 'success', message: '编辑成功' })
-          await getTableData()
-          closeAddUserDialog()
-        }
-      }
-    }
-  })
+const getAreaName = (e) => {
+  const item = areaList.value.find((item) => item.ID == e);
+  return item ? item.areaName : '未知';
+};
+
+const getPortStr =(e) => {
+  return e.join(',')
 }
 
-function getIpArr(e) {
-    if(e.includes(',')) {
-        return e.split(',')
-    }else {
-      return [e]
-    }
-}
-
-const districtInfoDialog = ref(false)
-const closeAddUserDialog = () => {
-  form.value.resetFields()
-  districtInfo.value.headerImg = ''
-  districtInfo.value.authorityIds = []
-  districtInfoDialog.value = false
-}
-
-const dialogFlag = ref('add')
-
-const addArea = () => {
-  dialogFlag.value = 'add'
-  districtInfoDialog.value = true
-  form.value.resetFields()
-}
-
-const handleClickUpdate = (row) => {
-  console.log(row)
-  row.areaIpStr = row.areaIp.join(',');
-  dialogFlag.value = 'edit'
-  districtInfo.value = JSON.parse(JSON.stringify(row))
-  districtInfoDialog.value = true
-}
+const getPortsTag = (e) => {
+   let arr = []
+   if(e.length > 0 ){
+     arr =  e.slice(0, 4);
+     return arr.join(',')
+   }else {
+    return e
+   }
+} 
 
 </script>
 
