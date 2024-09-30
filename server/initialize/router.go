baa9ef42801c82c71e216900fb2cef9e30a50446
丁元广 <dinyuanguang@163.com>
2024-09-30 10:27:20 +0800

    refactor: 上传过期证书

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -1,13 +1,13 @@
 package initialize
 
 import (
+	"codeup.aliyun.com/66d825f8c06a2fdac7bbfe8c/curescan/server/middleware"
 	"github.com/gin-contrib/pprof"
 	"net/http"
 	"os"
 
 	"codeup.aliyun.com/66d825f8c06a2fdac7bbfe8c/curescan/server/docs"
 	"codeup.aliyun.com/66d825f8c06a2fdac7bbfe8c/curescan/server/global"
-	"codeup.aliyun.com/66d825f8c06a2fdac7bbfe8c/curescan/server/middleware"
 	"codeup.aliyun.com/66d825f8c06a2fdac7bbfe8c/curescan/server/router"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
@@ -67,8 +67,8 @@ func Routers() *gin.Engine {
 
 	PrivateGroup := Router.Group(global.GVA_CONFIG.System.RouterPrefix)
 
-	PrivateGroup.Use(middleware.JWTAuth()).Use(middleware.CasbinHandler())
-	// PrivateGroup.Use(middleware.LicenseVerify()).Use(middleware.JWTAuth()).Use(middleware.CasbinHandler())
+	// PrivateGroup.Use(middleware.JWTAuth()).Use(middleware.CasbinHandler())
+	PrivateGroup.Use(middleware.LicenseVerify()).Use(middleware.JWTAuth()).Use(middleware.CasbinHandler())
 	// PublicGroup.Use(middleware.LicenseVerify())
 
 	{
