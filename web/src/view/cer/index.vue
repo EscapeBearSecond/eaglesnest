fa0492da561d487877ad558cbd3323bb3b7ceebd
chenrui <ruiweilai@163.com>
2024-09-30 11:05:02 +0800

    fix:证书导入页面拦截

--- web/src/view/cer/index.vue
+++ web/src/view/cer/index.vue
@@ -10,13 +10,46 @@
             </div>
             <hr style=" height:2px;border:none;border-top:2px dotted #185598;" />
             <div class="btn">
-                <el-button type="primary">上传证书</el-button>
+              <el-descriptions-item label-align="center" label="证书导入">
+                <el-upload
+                    v-model:file-list="fileList"
+                    class="upload-demo"
+                    action="''"
+                    multiple
+                    :limit="1"
+                    :before-upload="handleCustomUpload"
+                >
+                <el-button size="small" type="primary">上传证书</el-button>
+                <template #tip>
+                    <div class="el-upload__tip">
+                        文件格式：json 类型, 大小不超过 200KB.
+                    </div>
+                    </template>
+                </el-upload>
+            </el-descriptions-item>
             </div>
     </div>
 </template>
 <script setup>
 import { ref, reactive } from 'vue' 
+import { uploadLicense } from '@/api/api'
+import router from '@/router/index'
 
+const handleCustomUpload = async(file) => {
+  const formData = new FormData();
+  formData.append('file', file);
+  console.log(file)
+  // 阻止默认的上传行为
+  let data = await uploadLicense(formData)
+  if (data.code === 0) {
+      getData()
+      ElMessage({ type: 'success', message: '更新成功，请重新登陆！' })
+      router.push({ name: 'Login', replace: true })
+  }else {
+      ElMessage({ type: 'error', message: '更新失败，请联系相关人员操作！' })
+  }
+  return false;
+}
 </script>
 <style lang='scss' scoped>
   .cer-box {
