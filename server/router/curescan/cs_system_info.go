b1f6c2dfa780748a4722498c7fff92ee32ae5773
丁元广 <dinyuanguang@163.com>
2024-09-30 15:02:31 +0800

    fix: 修复导入证书也要验证证书的死循环

--- server/router/curescan/cs_system_info.go
+++ server/router/curescan/cs_system_info.go
@@ -2,7 +2,6 @@ package curescan
 
 import (
 	v1 "codeup.aliyun.com/66d825f8c06a2fdac7bbfe8c/curescan/server/api/v1"
-	"codeup.aliyun.com/66d825f8c06a2fdac7bbfe8c/curescan/server/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,7 +12,7 @@ func (s *SystemInfoRouter) InitSystemInfoRouter(Router *gin.RouterGroup) {
 	systemInfoRouter := Router.Group("systemInfo")
 	systemInfoApi := v1.ApiGroupApp.CurescanApiGroup.SystemInfoApi
 	{
-		systemInfoRouter.GET("", systemInfoApi.GetSystemInfo).Use(middleware.LicenseVerify())
+		systemInfoRouter.GET("", systemInfoApi.GetSystemInfo)
 		systemInfoRouter.POST("/license", systemInfoApi.UpdateLicense)
 	}
 }
