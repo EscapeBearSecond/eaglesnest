57f27930cd3c6357ec62544c04a264e8b523dbd8
丁元广 <dinyuanguang@163.com>
2024-10-10 11:28:06 +0800

    fix: 修复因唯一键导致的策略和区域无法新建的问题

--- server/model/curescan/cs_area.go
+++ server/model/curescan/cs_area.go
@@ -9,7 +9,7 @@ import (
 type Area struct {
 	global.GvaModel
 	global.CsModel
-	AreaName string         `json:"areaName" gorm:"type:text;not null;unique;index;column:area_name;"`
+	AreaName string         `json:"areaName" gorm:"type:text;not null;index;column:area_name;"`
 	AreaIP   pq.StringArray `json:"areaIp" gorm:"type:text[];not null;column:area_ip;"`
 	AreaDesc string         `json:"areaDesc" gorm:"type:text;column:area_desc;"`
 }
